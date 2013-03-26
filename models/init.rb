1ad4648030257227ea0c77218520395216c455dd
Frederick Capovilla <frederick@localhost.(none)>
2013-03-26 19:53:41 -0400

    Add default settings

--- models/init.rb
+++ models/init.rb
@@ -12,3 +12,10 @@ require_relative 'item'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+# Set default settings
+if Setting.all.count == 0
+	Setting.create(:name => 'username', :value => 'admin')
+	Setting.create(:name => 'salt', :value => '')
+	Setting.create(:name => 'password', :value => Digest::SHA512.hexdigest('admin'))
+end
