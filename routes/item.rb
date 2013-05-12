d9b9762016fdd4a0d61921f17ad933f2bec1340d
Frederick Capovilla <caposite@hotmail.com>
2013-05-12 15:25:01 +0000

    Add validations to items

--- routes/item.rb
+++ routes/item.rb
@@ -24,8 +24,15 @@ end
 
 put '/item/:id', '/feed/*/item/:id', '/folder/*/item/:id' do
 	item = Item.first(:user => @user, :id => params[:id])
-	item.attributes = JSON.parse(request.body.string, :symbolize_names => true)
-	item.save
+	attributes = JSON.parse(request.body.string, :symbolize_names => true)
+
+	item.attributes = attributes.slice(:read)
+
+	unless item.save
+		errors = item.errors.map{|e| e.first.to_s}
+		return 400, errors.to_json
+	end
+
 	item.feed.update_unread_count!
 	item.to_json
 end
