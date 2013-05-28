21e685ab1a4b2559f9debd452d057c7ee75a93d6
Frédérick Capovilla <frederick@frederick-XPS-8300.(none)>
2013-05-27 21:27:35 -0400

    Add app specs for sass and js files

---
+++ spec/routes/app_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../spec_helper'
+
+describe "Sinatra application" do
+
+	it 'compiles the sass stylesheet' do
+		get '/stylesheet.css'
+		last_response.status.should == 200
+		last_response.body.should =~ /\.feed-list/
+	end
+
+	it 'concatenates all the backbone app files' do
+		get '/app.js'
+		last_response.status.should == 200
+		last_response.body.should =~ /function formatDate/
+		last_response.body.should =~ /var Router/
+		last_response.body.should =~ /var Item/
+		last_response.body.should =~ /var ItemCollection/
+		last_response.body.should =~ /var ItemView/
+		last_response.body.should =~ /var ItemListView/
+	end
+
+end
