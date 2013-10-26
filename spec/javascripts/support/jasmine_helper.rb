bf86a5b1527fa9fd91716e566a7ecaf2dfd91846
frederick <frederick@localhost.localdomain>
2013-10-26 10:52:38 -0400

    Improve Jasmine helper to include underscore templates for view tests

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -1,8 +1,22 @@
+require 'haml'
+require 'r18n-core'
+
+R18n.default_places = 'i18n/'
+R18n.set('en')
+
+include R18n::Helpers
+
 module Jasmine
 	class Page
 		def render
 			template = File.read(File.join(File.dirname(__FILE__), "run.html.erb"))
 			ERB.new(template).result(@context.instance_eval { binding })
+		end
+	end
+
+	class Configuration
+		def haml(path)
+			Haml::Engine.new(File.read(path)).render
 		end
 	end
 end
